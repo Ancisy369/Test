bbbbb7b435f7e8beb4db23f5a6af06ccf1691474
Trần Đại Nghĩa <showmeyourtrueself@gmail.com>
2023-09-25 04:31:49 +0700

    hello

---
+++ Java-jdbc-8-2023/src/main/java/com/laptrinhjavaweb/controller/BuildingController.java
@@ -0,0 +1,31 @@
+package com.laptrinhjavaweb.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.laptrinhjavaweb.dto.BuildingDTO;
+import com.laptrinhjavaweb.input.BuildingSearchInput;
+import com.laptrinhjavaweb.output.BuildingOutput;
+import com.laptrinhjavaweb.service.BuildingService;
+import com.laptrinhjavaweb.service.emyeuanh.BuildingEmyeuanh;
+import com.laptrinhjavaweb.service.impl.BuildingServiceImpl;
+
+public class BuildingController {
+	private BuildingService buildingService = new BuildingServiceImpl();
+			
+	public List<BuildingOutput> findBuilding(BuildingSearchInput buildingSearch) {
+		List<BuildingOutput> results = buildingService.findBuilding(buildingSearch);
+		/*List<BuildingEmyeuanh> buildingEmyeuanhs = buildingService.findBuilding(buildingSearch);
+		
+		for(BuildingEmyeuanh item:buildingEmyeuanhs) {
+			BuildingOutput buildingModel = new BuildingOutput();
+			buildingModel.setName(item.getName());
+			results.add(buildingModel);
+		}*/
+		return results;
+	}
+	
+
+	
+
+}
