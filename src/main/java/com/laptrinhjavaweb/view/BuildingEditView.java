47f65e9e0eb1627efc54bccbc85d065a5c147524
Trần Đại Nghĩa <showmeyourtrueself@gmail.com>
2023-09-25 20:51:56 +0700

    eadsfa

---
+++ src/main/java/com/laptrinhjavaweb/view/BuildingEditView.java
@@ -0,0 +1,15 @@
+package com.laptrinhjavaweb.view;
+
+import com.laptrinhjavaweb.controller.BuildingController;
+import com.laptrinhjavaweb.dto.BuildingDTO;
+
+public class BuildingEditView {
+
+	
+	public static void main(String[] args) {
+		BuildingController 	buildingController = new BuildingController();
+		BuildingDTO newBuilding = new BuildingDTO();
+		//save
+	
+	}
+}
